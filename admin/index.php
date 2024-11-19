2d685e0187acbcbbae28c22e2862f8f419274b58
Duc-phu2k04 <nguyenducphu2k4@gmail.com>
2024-11-19 21:15:27 +0700

    Initial commit

---
+++ admin/index.php
@@ -0,0 +1,11 @@
+<?php
+include "../model/pdo.php";
+include "../model/danhmuc.php";
+include "../model/sanpham.php";
+include "../model/taikhoan.php";
+include "../model/binhluan.php";
+
+
+
+include "header.php";
+
---
+++ index.php
@@ -0,0 +1,9 @@
+<?php 
+session_start();
+include "model/pdo.php";
+include "model/sanpham.php";
+include "model/danhmuc.php";
+include "model/taikhoan.php";
+include "view/header.php";
+include "global.php";
+
